52ec5534944c09e5d7b6e56229deeca22d51a3f5
BogunD <90133397+BogunD@users.noreply.github.com>
2022-02-16 16:40:08 +0200

    Update functions.cpp

--- Lab1_OP/C++/functions.cpp
+++ Lab1_OP/C++/functions.cpp
@@ -1,47 +1,37 @@
 #include <iostream>
 #include<string>
 #include <fstream>
-#include <vector>
-std::vector<std::string> get_text() {
+std::string get_text() {
 	char ctrl_D = 4;
-	std::vector<std::string> lines;
-	std::string line = "";
+	std::string lines = {};
+	std::string line;
 	while (line[0] != ctrl_D) {
 		getline(std::cin, line);
-		lines.push_back(line);
+		lines += line + "\n";
 	}
-	lines.pop_back();
+	lines = lines.substr(0, lines.size() - 3);
 	return lines;
 }
-void fill_file(std::vector<std::string> text, std::string file) {
+void fill_file(std::string text, std::string file) {
 	std::ofstream fout(file, std::ios_base::out | std::ios_base::trunc);
 	if (!fout.is_open())
 		std::cout << "Error";
-	int i;
-	for (i = 0; i < text.size(); i++) {
-		fout << text[i]<<"\n";
-	}
+	fout << text;
 	fout.close();
 }
-std::vector<std::string> edit_file( std::string file_name) {
-	std::vector<std::string> text1, text2;
-	std::string	sent;
+std::string edit_file(std::string text, std::string file_name) {
+	std::string text1, text2, sent;
 	std::ifstream rfile(file_name, std::ios::in);
 	while (!rfile.eof()) {
 		getline(rfile, sent);
-		if (sent[0] != '#')
-			text1.push_back(sent);
+		if (sent[0] != '#') 
+			text1 += sent + "\n";
 		else {
 			sent = sent.substr(1, sent.size() / 2) + "!" + sent.substr((sent.size() / 2)+1, sent.size());
-			text2.push_back(sent);
+			text2 += sent + "\n";
 		}
 	}
-	text1.pop_back();
-	text1.insert(text1.end(), text2.begin(), text2.end());
+	text1 = text1.substr(0, text1.size() - 3);
+	text1 += "\n"+text2;
 	return text1;
-}
-void printVector(std::vector<std::string> name) {
-	for (int i = 0; i < name.size(); i++) {
-		std::cout << name[i]<<"\n";
-	}
 }
