9cf3b8548a7b1f72e8f4b4d77b8ad3a6e8e70b2e
BogunD <90133397+BogunD@users.noreply.github.com>
2022-02-16 16:41:38 +0200

    Update 2Lab1.cpp

--- Lab1_OP/C++/2Lab1.cpp
+++ Lab1_OP/C++/2Lab1.cpp
@@ -1,16 +1,18 @@
-﻿#include <iostream>
+#include <iostream>
 #include<string>
 #include <fstream>
 #include <vector>
 #include "functions.h"
 
 int main() {
-	std::string firstText = "Text.txt", secondText = "Text2.txt";
-	std::string text = get_text();
-	std::cout << "\nInput text:\n"<< text << "\n\n";
-	fill_file(text, firstText);
-	std::string text1 = edit_file(text,firstText);
-	std::cout << "Output text:\n" << text1 << "\n\n";
-	fill_file(text1, secondText);
+	std::string firstName = "Text.txt", secondName = "Text2.txt";
+	std::vector <std::string> text = get_text();
+	std::cout << "\nI N P U T   T E X T:\n";
+	printVector(text);
+	fill_file(text, firstName);
+	std::vector<std::string> text1 = edit_file(firstName);
+	std::cout << "\n\nO U T P U T   T E X T :\n";
+	printVector(text1);
+	fill_file(text1, secondName);
 }
 
