4d731f2bb54f7e4dd76b7f8c3ccc8b4772d7970d
BogunD <90133397+BogunD@users.noreply.github.com>
2022-02-17 08:57:28 +0200

    Update main.py

--- Lab1_OP/Python/main.py
+++ Lab1_OP/Python/main.py
@@ -1,13 +1,11 @@
 from functions import*
-text = inp_text()
-print("Input:", "\n")
-print(text)
+lines = inp_text()
+print("I N P U T   T E X T:", "\n")
+print_list(lines)
 name1 = "text.txt"
 name2 = "text2.txt"
-fill_file(text, name1)
-new_text = edit_file(name1)
-print("\nOutput:\n")
-print(new_text)
-fill_file(new_text, name2)
-
-
+fill_file(lines, name1)
+new_lines = edit_file(name1)
+print("\nO U T P U T   T E X T:\n")
+print_list(new_lines)
+fill_file(new_lines, name2)
